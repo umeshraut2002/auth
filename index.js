5499309d46d672c6ebff0f8d534dd2d99ab74721
umesh raut <uraut6044@gmail.com>
2025-03-22 15:14:39 +0530

    index js updated

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-import express from "express";
+import express, { application } from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+app.get("/",(req, res) => {
+    res.send("Response");
+})
+
+app.use
